0640bef53a473a00c1b167e497315630ad2d07fd
Kaylin <kzaroukian@gmail.com>
2019-01-20 01:19:58 -0500

    added gitignore

---
+++ BookPlaylistApp/Pods/Target Support Files/Pods-BookPlaylistAppTests/Pods-BookPlaylistAppTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_BookPlaylistAppTests : NSObject
+@end
+@implementation PodsDummy_Pods_BookPlaylistAppTests
+@end
---
+++ BookPlaylistApp/Pods/Target Support Files/Pods-BookPlaylistAppUITests/Pods-BookPlaylistAppUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_BookPlaylistAppUITests : NSObject
+@end
+@implementation PodsDummy_Pods_BookPlaylistAppUITests
+@end
